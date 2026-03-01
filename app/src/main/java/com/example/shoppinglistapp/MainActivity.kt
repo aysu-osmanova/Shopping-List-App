7ca42e4db45b98c07c6841a785a39cec876926a5
Aysu <aysuosmanova84@gmail.com>
2026-03-01 22:44:28 +0400

    Starting with the shopping list UI by adding a button and lazy column and data class

---
+++ app/src/main/java/com/example/shoppinglistapp/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.example.shoppinglistapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.shoppinglistapp.ui.theme.ShoppingListAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ShoppingListAppTheme {
+
+                var sItems by remember { mutableStateOf(listOf<ShoppingItem>()) }
+
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(modifier = Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding),
+                        verticalArrangement = Arrangement.Center
+                    ){
+                        Button(
+                            {},
+                            modifier = Modifier.align(Alignment.CenterHorizontally)
+                        ){
+                            Text("Add Item")
+                        }
+                        LazyColumn(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(16.dp)
+                        ){
+                            items(sItems){
+
+                            }
+                        }
+                    }
+
+
+                    }
+                }
+            }
+        }
+    }
+data class ShoppingItem( val id: Int,
+                         var name: String,
+                         var quantity: Int,
+                         var isEditing: Boolean=false
+
+)
