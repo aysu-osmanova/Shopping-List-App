7ac8a17e026b599638143b5f17b12bfc95444c48
Aysu <aysuosmanova84@gmail.com>
2026-03-05 22:17:30 +0400

    Upgraded the user interface of shopping list app with icon buttons

--- app/src/main/java/com/example/shoppinglistapp/ShoppingList.kt
+++ app/src/main/java/com/example/shoppinglistapp/ShoppingList.kt
@@ -1,16 +1,27 @@
 package com.example.shoppinglistapp
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -21,6 +32,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 data class ShoppingItem( val id: Int,
@@ -56,7 +68,7 @@ fun ShoppingListApp() {
                     .padding(16.dp)
             ) {
                 items(sItems) {
-
+                    ShoppingListItem(it, {}, {})
                 }
             }
         }
@@ -100,6 +112,73 @@ fun ShoppingListApp() {
                     }
                 }
             )
+        }
+    }
+}
+
+@Composable
+fun ShoppingItemEditor(item: ShoppingItem, onEditComplete:( String, Int )-> Unit){
+    var editedName by remember{ mutableStateOf(item.name) }
+    var editedQuantity by remember{ mutableStateOf(item.quantity.toString()) }
+    var isEditing by remember { mutableStateOf(item.isEditing) }
+
+    Row(modifier = Modifier.fillMaxWidth()
+        .background(Color.White)
+        .padding(8.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly
+        )
+    {
+        Column() {
+            BasicTextField(
+                value = editedName,
+                onValueChange = {editedName = it},
+                singleLine = true,
+                modifier = Modifier.wrapContentSize().padding(8.dp)
+            )
+            BasicTextField(
+                value = editedQuantity,
+                onValueChange = {editedQuantity = it},
+                singleLine = true,
+                modifier = Modifier.wrapContentSize().padding(8.dp)
+
+            )
+        }
+        Button(
+            {
+                isEditing = false
+                onEditComplete(editedName, editedQuantity.toIntOrNull() ?: 1)
+            }
+        ){
+            Text("Save")
+        }
+    }
+
+}
+
+
+@Composable
+fun ShoppingListItem(
+    item: ShoppingItem,
+    onEditClick: () -> Unit,
+    onDeleteClick: () -> Unit
+){
+    Row(
+        modifier = Modifier.padding(8.dp).fillMaxWidth().border(
+            border = BorderStroke(2.dp, Color(0xFF3F51B5)),
+            shape = RoundedCornerShape(30)
+        )
+    ){
+        Text(item.name, modifier = Modifier.padding(8.dp))
+        Text("Qty: ${item.quantity}", modifier = Modifier.padding(8.dp))
+
+        Row(modifier = Modifier.padding(8.dp)) {
+        IconButton(onEditClick) {
+            Icon(imageVector = Icons.Default.Edit, contentDescription = null)
+        }
+
+         IconButton(onDeleteClick) {
+             Icon(imageVector = Icons.Default.Delete, contentDescription = null)
+         }
         }
     }
 }
